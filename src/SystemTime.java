2d1a07eae4f5c53a8c6310421ac1cb52d3277a0c
106210740 <106210740@student.swin.edu.au>
2025-12-10 14:25:03 +0700

    thuchanh2

--- src/SystemTime.java
+++ src/SystemTime.java
@@ -1,4 +1,7 @@
-package PACKAGE_NAME;
-
+import java.util.Date;
 public class SystemTime {
+    public static void main(String[] args) {
+        Date now = new Date(); // lay tgian hien tai
+        System.out.println("Now is: " + now);
+    }
 }
